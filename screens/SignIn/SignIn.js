f66766016397ca87a8434e2e1646b2e3408aed4c
j25nkh <j25nkh@gmail.com>
2023-04-13 21:30:14 +0900

    feat: 구글 소셜 로그인 기능 구현
    - expo-auth-session을 이용한 구글 소셜로그인 기능 구현

---
+++ screens/SignIn/SignIn.js
@@ -0,0 +1,72 @@
+import {
+  REACT_APP_WEB_CLIENT_ID,
+  REACT_APP_ANDROID_CLIENT_ID,
+  REACT_APP_IOS_CLIENT_ID,
+} from "@env";
+import * as Google from "expo-auth-session/providers/google";
+import * as WebBrowser from "expo-web-browser";
+import { useEffect, useState } from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { useDispatch } from "react-redux";
+
+import styles from "./styles";
+import logo from "../../assets/images/logo.png";
+import splash from "../../assets/images/sign-in-background.png";
+import { setUserInfo } from "../../redux/userSlice";
+
+WebBrowser.maybeCompleteAuthSession();
+
+export default function SignIn() {
+  const [accessToken, setAccessToken] = useState("");
+  const dispatch = useDispatch();
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: REACT_APP_WEB_CLIENT_ID,
+    androidclientIdd: REACT_APP_ANDROID_CLIENT_ID,
+    iosclientI: REACT_APP_IOS_CLIENT_ID,
+    prompt: "select_account",
+  });
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      setAccessToken(response.authentication.accessToken);
+      getUserInfo();
+    }
+  }, [request, response, accessToken]);
+
+  const getUserInfo = async () => {
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: { Authorization: `Bearer ${accessToken}` },
+        },
+      );
+
+      const user = await response.json();
+      dispatch(setUserInfo(user));
+    } catch (error) {
+      console.warn(error);
+    }
+  };
+
+  const handlePress = () => {
+    promptAsync();
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.backgroundContainer}>
+        <Image source={splash} style={styles.background} />
+        <View style={styles.logoContainer}>
+          <Image source={logo} style={styles.logo} />
+        </View>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={handlePress}>
+          <Text>구글계정으로 시작하기</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
