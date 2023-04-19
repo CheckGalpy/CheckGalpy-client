aa023a1b9d106f2c57c9ab38e0c0f1bc761a0755
j25nkh <j25nkh@gmail.com>
2023-04-20 05:17:15 +0900

    feat: 책갈피 열람 / 필터 / 검색 기능 추가
    - 책갈피 페이지 구현
    - GET api/bookmark/creator 쿼리를 통해 책갈피 전체 열람기능 구현
    - 해시태그 검색기능 구현
    - 책갈피 최신순/오래된순 필터 기능 추가

--- screens/Bookmark/Bookmark.js
+++ screens/Bookmark/Bookmark.js
@@ -1,14 +1,171 @@
-import { View, Text } from "react-native";
-import { useSelector } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  View,
+  TextInput,
+  Text,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  Alert,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { REACT_APP_API_URI as API_URI } from "@env";
 
+import { setBookmarkId } from "../../redux/currentBookmarkSlice";
+import transformTimeStamp from "../../utils/formatTimeStamp";
 import styles from "./styles";
 
 export default function Bookmark() {
-  const text = useSelector((state) => state.text.textInfo);
+  const { navigate } = useNavigation();
+
+  const dispatch = useDispatch();
+  const userId = useSelector((state) => state.user.userId);
+
+  const [bookmarkList, setBookmarkList] = useState([]);
+  const [query, setQuery] = useState("");
+  const [activeTab, setActiveTab] = useState("전체");
+  const [sortOrder, setSortOrder] = useState("최신순");
+
+  useEffect(() => {
+    const getAllMyBookmarks = async () => {
+      try {
+        const url = `${API_URI}/api/bookmark/creator?creatorId=${userId}`;
+        const response = await fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const bookmarkList = await response.json();
+        setBookmarkList(bookmarkList);
+      } catch (error) {
+        console.warn(error);
+      }
+    };
+    getAllMyBookmarks();
+  }, [activeTab]);
+
+  const handleSearch = () => {
+    const filteredBookmarks = bookmarkList.filter((bookmark) =>
+      bookmark.hashtags.includes(query),
+    );
+    setBookmarkList(filteredBookmarks);
+  };
+
+  const handleTabSwitch = (tab) => {
+    setActiveTab(tab);
+  };
+
+  const handleSortToggle = () => {
+    const reversedBookmarkList = bookmarkList.reverse();
+    setBookmarkList(reversedBookmarkList);
+
+    if (sortOrder === "최신순") {
+      setSortOrder("오래된순");
+    } else {
+      setSortOrder("최신순");
+    }
+  };
+
+  const handleBookmarkCardPress = (bookmarkId) => {
+    dispatch(setBookmarkId(bookmarkId));
+    navigate("BookmarkDetail");
+  };
+
+  const handleDeleteBookmark = async (bookmarkId) => {
+    const response = await fetch(`${API_URI}/api/bookmark/${bookmarkId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.status === 200) {
+      Alert.alert("책갈피가 삭제되었습니다!");
+    } else {
+      Alert.alert("책갈피 삭제에 실패하였습니다");
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <Text>{text}</Text>
+      <View style={styles.searchContainer}>
+        <Image
+          source={require("../../assets/images/icon-lense.png")}
+          style={styles.lenseIcon}
+          resizeMode="contain"
+        />
+        <TextInput
+          placeholder="해시태그를 검색 해보세요"
+          value={query}
+          onChangeText={setQuery}
+          onSubmitEditing={handleSearch}
+          style={styles.searchInput}
+        />
+      </View>
+      <View style={styles.controllerContainer}>
+        <View style={styles.selectionContainer}>
+          <TouchableOpacity
+            style={[styles.tab, activeTab === "전체" && styles.activeTab]}
+            onPress={() => handleTabSwitch("전체")}
+          >
+            <Text style={styles.tabText}>전체</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.tab, activeTab === "좋아요" && styles.activeTab]}
+            onPress={() => handleTabSwitch("좋아요")}
+          >
+            <Text style={styles.tabText}>좋아요</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.filterContainer}>
+          <TouchableOpacity
+            style={styles.filterInnerContainer}
+            onPress={handleSortToggle}
+          >
+            <Text style={styles.filterText}>{sortOrder}</Text>
+            <Image
+              source={require("../../assets/images/icon-filter.png")}
+              style={styles.filterIcon}
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.cardContainer}>
+        <ScrollView>
+          {bookmarkList.map((bookmark, index) => (
+            <View key={index} style={styles.card}>
+              <TouchableOpacity
+                onPress={() => handleBookmarkCardPress(bookmark._id)}
+              >
+                <Text style={styles.dateString}>
+                  {transformTimeStamp(new Date(bookmark.createdAt))}
+                </Text>
+                <Text style={styles.content}>{bookmark.content}</Text>
+                <View style={styles.cardBottomConntainer}>
+                  <View style={styles.hashtagContainer}>
+                    {bookmark.hashtags.map((tag, index) => (
+                      <Text style={styles.hashtag} key={index}>
+                        #{tag}
+                      </Text>
+                    ))}
+                  </View>
+                  <View style={styles.deleteButtonContainer}>
+                    <TouchableOpacity
+                      style={styles.deleteButton}
+                      onPress={() => handleDeleteBookmark(bookmark._id)}
+                    >
+                      <Text style={styles.deleteText}>삭제</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 }
--- screens/Bookmark/styles.js
+++ screens/Bookmark/styles.js
@@ -5,7 +5,122 @@ import { PRIMARY_YELLOW } from "../../constants/colors";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 10,
+    backgroundColor: "#fff",
+  },
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderTopColor: "#e1e1e1",
+    borderBottomColor: "#e1e1e1",
+  },
+  lenseIcon: {
+    width: 35,
+    height: 35,
+    marginLeft: 10,
+  },
+  searchInput: {
+    flex: 1,
+    height: 50,
+    paddingHorizontal: 16,
+  },
+  controllerContainer: {
+    flexDirection: "row",
+    height: 50,
+    borderBottomWidth: 1,
+    borderBottomColor: "#e1e1e1",
+  },
+  selectionContainer: {
+    flex: 4,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingLeft: 10,
+  },
+  tab: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderTopLeftRadius: 10,
+    borderBottomRightRadius: 10,
+  },
+  activeTab: {
     backgroundColor: PRIMARY_YELLOW,
+  },
+  tabText: {
+    fontSize: 14,
+    fontWeight: "600",
+    color: "black",
+  },
+  filterContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingRight: 10,
+  },
+  filterInnerContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  filterText: {
+    fontSize: 10,
+    fontcolor: "#e1e1e1",
+  },
+  filterIcon: {
+    width: 25,
+    height: 25,
+    marginLeft: 10,
+  },
+  cardContainer: { paddingTop: 5, paddingBottom: 105 },
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 5,
+    marginRight: 10,
+    marginLeft: 10,
+    marginBottom: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 5,
+    elevation: 6,
+  },
+  dateString: {
+    marginBottom: 20,
+    fontSize: 14,
+    fontWeight: "600",
+  },
+  content: {
+    marginBottom: 20,
+    fontSize: 14,
+  },
+  cardBottomConntainer: { flexDirection: "row" },
+  hashtagContainer: {
+    flex: 7,
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  hashtag: {
+    marginRight: 10,
+    marginBottom: 5,
+    padding: 5,
+    borderRadius: 10,
+    backgroundColor: "ghostwhite",
+    fontSize: 12,
+  },
+  deleteButtonContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "flex-end",
+  },
+  deleteText: {
+    color: "gray",
+    fontSize: 10,
   },
 });
 
