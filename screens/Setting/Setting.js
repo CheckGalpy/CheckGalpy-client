305ac22fe977d1eef16014d58da71fca2cafbb6f
j25nkh <j25nkh@gmail.com>
2023-04-23 01:24:16 +0900

    feat: 설정 페이지 구현
    - 내 정보 보이기 기능 구현
    - 로그아웃 기능 배치

--- screens/Setting/Setting.js
+++ screens/Setting/Setting.js
@@ -1,13 +1,36 @@
-import { useDispatch } from "react-redux";
-import { View, Text, TouchableOpacity } from "react-native";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import * as SecureStore from "expo-secure-store";
+import { REACT_APP_API_URI as API_URI } from "@env";
 
-import { setAuthStatus } from "../../redux/authSlice";
+import { setAuthStatus } from "../../redux/authStatusSlice";
 
 import styles from "./styles";
 
 export default function Setting() {
   const dispatch = useDispatch();
+  const currentUserId = useSelector(
+    (state) => state.currentUserId.currentUserId,
+  );
+
+  const [currentUser, setCurrentUser] = useState();
+
+  useEffect(() => {
+    const getcurrentUser = async () => {
+      const response = await fetch(`${API_URI}/api/users/${currentUserId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const currentUser = await response.json();
+      setCurrentUser(currentUser);
+    };
+
+    getcurrentUser();
+  }, [currentUserId]);
 
   const handleLogout = async () => {
     await SecureStore.deleteItemAsync("token");
@@ -16,11 +39,24 @@ export default function Setting() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} onPress={handleLogout}>
-          <Text>로그아웃</Text>
-        </TouchableOpacity>
+      <View style={styles.profileContainer}>
+        <View style={styles.avatarContainer}>
+          <Image
+            style={styles.avatar}
+            source={{ uri: currentUser?.avatarURL }}
+            resizeMode="contain"
+          />
+        </View>
+        <View style={styles.userInfoContainer}>
+          <Text style={styles.userName}>
+            {currentUser?.familyName + currentUser?.givenName}
+          </Text>
+          <Text style={styles.email}>{currentUser?.email}</Text>
+        </View>
       </View>
+      <TouchableOpacity style={styles.buttonContainer} onPress={handleLogout}>
+        <Text>로그아웃</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- screens/Setting/styles.js
+++ screens/Setting/styles.js
@@ -6,12 +6,58 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
+    alignItems: "center",
   },
-  button: {
+  profileContainer: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 20,
+    marginTop: 5,
+    borderTopWidth: 1,
+    borderTopColor: "#e1e1e1",
+    borderBottomWidth: 1,
+    borderBottomColor: "#e1e1e1",
+  },
+  avatarContainer: {
+    flex: 1,
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  avatar: {
+    width: 46,
+    height: 46,
+    borderRadius: 23,
+  },
+  userInfoContainer: {
+    flex: 4,
+  },
+  userInfo: {
+    flex: 3,
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+  userName: {
+    marginBottom: 4,
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  email: {
+    marginBottom: 4,
+    fontSize: 12,
+    color: "gray",
+  },
+  buttonContainer: {
     backgroundColor: PRIMARY_YELLOW,
     borderRadius: 30,
     paddingVertical: 15,
     paddingHorizontal: 40,
+    width: "90%",
+    alignItems: "center",
+    marginTop: 25,
   },
 });
 
