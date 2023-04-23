d013bcbcdd27e4f6d750c7d347533198ad01a1d5
j25nkh <j25nkh@gmail.com>
2023-04-23 23:00:49 +0900

    feat: 팔로잉 스크린 / 기능구현
    - 팔로잉 계정정보 디스플레이 구현
    - 검색에 따른 팔로잉 목록 업데이트 기능 구현
    - 정렬순에 따른 팔로잉 목록 업데이트 기능 구현

--- screens/Following/Following.js
+++ screens/Following/Following.js
@@ -1,11 +1,153 @@
-import { View, Text } from "react-native";
+import React, { useState, useEffect, useCallback, useRef } from "react";
+import { useSelector } from "react-redux";
+import { View, Text, Image, TouchableOpacity, ScrollView } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
+import { REACT_APP_API_URI as API_URI } from "@env";
 
 import styles from "./styles";
 
+import SearchBox from "../../components/SearchBox.js/SearchBox";
+import SortController from "../../components/SortController/SortController";
+
 export default function Following() {
+  const currentUserId = useSelector(
+    (state) => state.currentUserId.currentUserId,
+  );
+
+  const scrollViewRef = useRef();
+
+  const [refresh, setRefresh] = useState(false);
+  const [followingList, setFollowingList] = useState([]);
+  const [followingStatusList, setFollowingStatusList] = useState({});
+  const [filteredFollowingList, setFilteredFollowingList] = useState([]);
+  const [isSearching, setIsSearching] = useState(false);
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const [sortOrder, setSortOrder] = useState("오름차순");
+
+  useEffect(() => {
+    getAllFollowings();
+  }, [refresh]);
+
+  useEffect(() => {
+    if (searchKeyword) {
+      handleSearch();
+    } else {
+      setIsSearching(false);
+    }
+  }, [searchKeyword]);
+
+  useFocusEffect(
+    useCallback(() => {
+      setRefresh((prevRefresh) => !prevRefresh);
+    }, []),
+  );
+
+  const getAllFollowings = async () => {
+    try {
+      const url = `${API_URI}/api/follows/${currentUserId}/following`;
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const followingUsers = await response.json();
+      const sortedFollowingUsers = followingUsers.sort((a, b) =>
+        a.familyName.toLowerCase() > b.familyName.toLowerCase() ? 1 : -1,
+      );
+      setFollowingList(sortedFollowingUsers);
+
+      const followingStatus = sortedFollowingUsers.reduce((acc, user) => {
+        acc[user._id] = true;
+        return acc;
+      }, {});
+
+      setFollowingStatusList(followingStatus);
+    } catch (error) {
+      console.error("팔로잉 정보를 가져오는데 실패하였습니다: ", error);
+    }
+  };
+
+  const handleSearch = async () => {
+    const filteredFollowingList = followingList.filter(
+      (following) =>
+        following.email.includes(searchKeyword) ||
+        (following.familyName + following.givenName).includes(searchKeyword),
+    );
+    setFilteredFollowingList(filteredFollowingList);
+    setIsSearching(true);
+    scrollToTop();
+  };
+
+  const handleSortToggle = () => {
+    setFollowingList(followingList.reverse());
+    setFilteredFollowingList(filteredFollowingList.reverse());
+    setSortOrder(sortOrder === "오름차순" ? "내림차순" : "오름차순");
+    scrollToTop();
+  };
+
+  const scrollToTop = () => {
+    scrollViewRef.current.scrollTo({ x: 0, y: 0, animated: false });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Following</Text>
+      <SearchBox
+        setSearchKeyword={setSearchKeyword}
+        placeholder="팔로우하는 계정을 검색해보세요"
+      />
+      <View style={styles.controllerContainer}>
+        <View style={styles.selectionContainer}>
+          <TouchableOpacity style={[styles.tab]}>
+            <Text style={styles.tabText}>팔로잉</Text>
+          </TouchableOpacity>
+        </View>
+        <SortController
+          sortOrder={sortOrder}
+          handleSortToggle={handleSortToggle}
+        />
+      </View>
+      <View style={styles.cardContainer}>
+        <ScrollView ref={scrollViewRef}>
+          {(isSearching ? filteredFollowingList : followingList).map(
+            (targetUser, index) => (
+              <View key={index} style={styles.card}>
+                <TouchableOpacity
+                  style={styles.profileContainer}
+                  // onPress={() => handleFollowCardPress(targetUser._id)}
+                >
+                  <View style={styles.avatarContainer}>
+                    <Image
+                      style={styles.avatar}
+                      source={{ uri: targetUser.avatarURL }}
+                      resizeMode="contain"
+                    />
+                  </View>
+                  <View style={styles.userInfoContainer}>
+                    <Text style={styles.userName}>
+                      {targetUser.familyName + targetUser.givenName}
+                    </Text>
+                    <Text style={styles.email}>{targetUser.email}</Text>
+                  </View>
+                </TouchableOpacity>
+                <View style={styles.followingButtonContainer}>
+                  <TouchableOpacity style={styles.followingingButtonContainer}>
+                    <Image
+                      source={
+                        followingStatusList[targetUser._id]
+                          ? require("../../assets/images/button-unfollow.png")
+                          : require("../../assets/images/button-follow.png")
+                      }
+                      style={styles.followingButton}
+                      resizeMode="contain"
+                    />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            ),
+          )}
+        </ScrollView>
+      </View>
     </View>
   );
 }
--- screens/Following/styles.js
+++ screens/Following/styles.js
@@ -1,11 +1,98 @@
 import { StyleSheet } from "react-native";
 
-import { PRIMARY_YELLOW } from "../../constants/colors";
+import { PRIMARY_BLUE } from "../../constants/colors";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: PRIMARY_YELLOW,
+    paddingTop: 10,
+    backgroundColor: "#fff",
+  },
+  controllerContainer: {
+    flexDirection: "row",
+    height: 50,
+    borderBottomWidth: 1,
+    borderBottomColor: "#e1e1e1",
+  },
+  selectionContainer: {
+    flex: 4,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingLeft: 10,
+  },
+  tab: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderTopLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    backgroundColor: PRIMARY_BLUE,
+  },
+  tabText: {
+    fontSize: 14,
+    fontWeight: "600",
+    color: "white",
+  },
+  cardContainer: { paddingTop: 5, paddingBottom: 105 },
+  card: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 5,
+    marginRight: 10,
+    marginBottom: 10,
+    marginLeft: 10,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: "#fff",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 5,
+    elevation: 6,
+  },
+  profileContainer: {
+    flex: 6,
+    flexDirection: "row",
+  },
+  avatarContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  avatar: {
+    width: 46,
+    height: 46,
+    borderRadius: 23,
+  },
+  userInfoContainer: {
+    flex: 4,
+  },
+  userInfo: {
+    flex: 3,
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+  userName: {
+    marginBottom: 4,
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  email: {
+    marginBottom: 4,
+    fontSize: 12,
+    color: "gray",
+  },
+  followingButtonContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  followingButton: {
+    width: 42,
+    height: 42,
   },
 });
 
