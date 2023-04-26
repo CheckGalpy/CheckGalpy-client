af962820bc1fa2ac6050697dbec68b5e0ee196f7
j25nkh <j25nkh@gmail.com>
2023-04-26 22:34:13 +0900

    feat: 손가락 오므리기 모션을 통해 책갈피 본문 줄바꿈 제거기능 구현

--- components/EditableContent/EditableContent.js
+++ components/EditableContent/EditableContent.js
@@ -1,38 +1,74 @@
-import { useState } from "react";
-import { Text, TextInput, TouchableOpacity } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { PinchGestureHandler, State } from "react-native-gesture-handler";
 import PropTypes from "prop-types";
 
 import styles from "./styles";
 
 export default function EditableContent({ content, onContentChange }) {
   const [editingContent, setEditingContent] = useState(false);
+  const [localContent, setLocalContent] = useState(content);
+
+  useEffect(() => {
+    setLocalContent(content);
+  }, [content]);
 
   const handlePress = () => {
     setEditingContent(true);
   };
 
   const handleBlur = () => {
+    onContentChange(localContent);
     setEditingContent(false);
   };
 
   const handleContentChange = (newText) => {
-    onContentChange(newText);
+    setLocalContent(newText);
+  };
+
+  const removeLineBreaks = () => {
+    const newContent = localContent
+      .split(/(\r\n|\n|\r)/gm)
+      .map((line) => line.trim())
+      .filter((line) => line.length > 0)
+      .join(" ");
+    setLocalContent(newContent);
+  };
+
+  const onPinchEvent = ({ nativeEvent }) => {
+    if (nativeEvent.scale < 0.99) {
+      removeLineBreaks();
+      console.warn("핀치 모션이 감지되었습니다. 여백을 삭제를 진행합니다.");
+    }
+  };
+
+  const onPinchStateChange = (event) => {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      removeLineBreaks();
+    }
   };
 
   return (
     <>
       {editingContent ? (
-        <TextInput
-          value={content}
-          onChangeText={handleContentChange}
-          onBlur={handleBlur}
-          autoFocus
-          multiline
-          style={styles.content}
-        />
+        <PinchGestureHandler
+          onGestureEvent={onPinchEvent}
+          onHandlerStateChange={onPinchStateChange}
+        >
+          <View style={styles.container}>
+            <TextInput
+              value={localContent}
+              onChangeText={handleContentChange}
+              onBlur={handleBlur}
+              autoFocus
+              multiline
+              style={styles.content}
+            />
+          </View>
+        </PinchGestureHandler>
       ) : (
         <TouchableOpacity onPress={handlePress}>
-          <Text style={styles.content}>{content}</Text>
+          <Text style={styles.content}>{localContent}</Text>
         </TouchableOpacity>
       )}
     </>
